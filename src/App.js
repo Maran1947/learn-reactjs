cc6f921337d639146ba5ce156c18d0fe8122014a
Maran1947 <abhishekmaran1947@gmail.com>
2021-10-13 11:16:58 +0530

    changes app.js and search.css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Counter from './project/counter/counter';
+import Counter from './project/counter/Counter';
 import quote_generator from './project/quote_generator/Quote_Generator';
 import Home from './project/home/Home';
 import Search from './project/search/Search'
