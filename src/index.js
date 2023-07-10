753009775a49ea47c93dbd52292f2fd32b9abed9
Tu Nombre <tu@email.com>
2023-07-10 07:59:18 -0400

    I added a motivational phrase as a practice

--- src/index.js
+++ src/index.js
@@ -15,3 +15,13 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+/*"El éxito no es un destino, es un viaje. No importa cuántos obstáculos encuentres en el camino, sigue adelante con determinación. Cada desafío es una oportunidad para crecer y aprender. No te rindas ante las dificultades, porque dentro de ti hay una fuerza infinita que te impulsa hacia adelante.
+
+Recuerda que los errores no son fracasos, son lecciones valiosas en tu camino hacia el logro. Aprovecha cada tropiezo como una oportunidad para mejorar y avanzar más fuerte. Mantén tus metas en mente y enfócate en el progreso constante.
+
+Confía en ti mismo y en tus habilidades. Tienes dentro de ti todo lo que necesitas para triunfar. Cree en tus sueños y trabaja arduamente para convertirlos en realidad. No permitas que el miedo te detenga, enfréntalo con valentía y determinación.
+
+Recuerda siempre el poder de una actitud positiva. La forma en que enfrentas los desafíos determina tu éxito. Mantén tu mente enfocada en lo positivo, en las soluciones en lugar de los problemas. Convierte los obstáculos en oportunidades y nunca pierdas de vista tu visión.
+
+Así que sigue adelante, persiste y nunca te des por vencido. En cada paso del camino, recuerda que eres capaz de grandes cosas. ¡Tú tienes el poder de hacer que tus sueños se hagan realidad!" */
